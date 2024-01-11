9e91008a9478f7dfa828d20c363175157d81557c
MrudaniPimpalkhare <139698773+MrudaniPimpalkhare@users.noreply.github.com>
2024-01-11 15:40:13 +0530

    Create Lab1c.ino

---
+++ Lab1c.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("Hello, ESP32!");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println(random(0,100));
+  delay(1000); // this speeds up the simulation
+}
