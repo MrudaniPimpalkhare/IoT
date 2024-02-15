a68eef5556d8cd2a863ab235188144a79b6bba2b
MrudaniPimpalkhare <139698773+MrudaniPimpalkhare@users.noreply.github.com>
2024-02-15 15:52:02 +0530

    Update part2.ino

--- Lab5/part2.ino
+++ Lab5/part2.ino
@@ -1,40 +1,41 @@
 #include "DHT.h"
 #include <Servo.h>
 #define DHTTYPE DHT11
-#define dht_dpin 3
+#define dht_dpin 4
 Servo servo1;
-const int servoPin = 4;
+const int servoPin = 13;
 
 float temp;
 float humidity;
 DHT dht(dht_dpin, DHTTYPE);
 float deg2rad(float in) {
-    return in * M_PI / 180.0;
+  return in * M_PI / 180.0;
 }
 void setup() {
   // put your setup code here, to run once:
-  
+
   Serial.begin(115200);
- 
+
   dht.begin();
   servo1.attach(
-        servoPin, 
-        Servo::CHANNEL_NOT_ATTACHED,
-        deg2rad(2.00),
-        deg2rad(179.00)
-    );
+    servoPin,
+    0
 
+  );
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
   humidity = dht.readHumidity();
   temp = dht.readTemperature();
-  float frac_t = (temp-30)/(3*180.0);
-  float frac_h = (humidity-42)/(32*180.0);
-  servo1.write(deg2rad(frac_h));
-  Serial.println(deg2rad(frac_h));
+  float frac_t = (temp - 30) * 60;
+  float frac_h = ((humidity - 48) / 32) * 180.0;
+  servo1.write((frac_h));
+  Serial.println((frac_h));
+  Serial.print(" ");
+  Serial.print(humidity);
+  Serial.print(" ");
+  Serial.println(temp);
   delay(1000);
-    
-
 }
