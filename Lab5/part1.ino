54599415468c6f75e93058bfa6c37fb1df7dc9d3
MrudaniPimpalkhare <139698773+MrudaniPimpalkhare@users.noreply.github.com>
2024-02-15 14:48:21 +0530

    Update part1.ino

--- Lab5/part1.ino
+++ Lab5/part1.ino
@@ -1,28 +1,42 @@
 #include "DHT.h"
 #define DHTTYPE DHT11
-#define dht_dpin 3
+#define dht_dpin 4
 float temp;
+float t_mean = 0;
+float h_mean = 0;
 float humidity;
-DHT dht(dht_dpn, DHTTYPE)
+DHT dht(dht_dpin, DHTTYPE);
 
 void setup() {
   // put your setup code here, to run once:
-  
-  Serial.begin(115200);\
+
+  Serial.begin(115200);
   Serial.println(F("DHTxx test!"));
   dht.begin();
-
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  humidity = dht.readHumidity();
-  temp = dht.readTemperature();
-  Serial.print("temperature:")
-  Serial.println(temp);
-  Serial.print("humidity");
-  Serial.println(humidity);
+
+  for (int i = 0; i < 20; i++) {
+    humidity = dht.readHumidity();
+    temp = dht.readTemperature();
+    // Serial.print("temperature:");
+    Serial.print(temp);
+    Serial.print("\t");
+    // Serial.print("humidity:");
+    Serial.println(humidity);
+    t_mean += temp;
+    h_mean += humidity;
+    delay(3000);
+  }
+  t_mean = t_mean/20;
+  h_mean = h_mean/20;
+  Serial.print("tempMean:");
+  Serial.print(t_mean);
+  Serial.print("\t");
+  Serial.print("hMean:");
+  Serial.println(h_mean);
+
   delay(1000);
-
-
 }
