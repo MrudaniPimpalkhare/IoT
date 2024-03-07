537fdbfe2392bffdea8484de594241bdcda703cf
MrudaniPimpalkhare <139698773+MrudaniPimpalkhare@users.noreply.github.com>
2024-03-07 15:59:29 +0530

    Update 2A.ino

--- Lab6/2A.ino
+++ Lab6/2A.ino
@@ -1,24 +1,22 @@
 #include <WiFi.h>
-#include <WebServer.h>
-#define sensor 2
+
+char* ssid = "mruda";
+char* password = "12345678";
 
 WiFiServer server(80);
-
-const char* ssid = "mruda";
-const char* pwd = "12345678";
-int status = WL_IDLE_STATUS;
-
+String header;
+String output12State = "off";
+int output12 = 2;
 
 void setup() {
-  delay(500);
   Serial.begin(115200);
-  // pinMode(ledPin, OUTPUT);
-  pinMode(sensor, OUTPUT);
+  pinMode(output12, OUTPUT);
+  digitalWrite(output12, LOW);
 
   Serial.print("Connecting to ");
   Serial.println(ssid);
 
-  WiFi.begin(ssid, pwd);
+  WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -26,12 +24,10 @@ void setup() {
   }
 
   Serial.println("");
-  Serial.println("WiFi connected");
+  Serial.println("WiFi connected.");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
-
   server.begin();
-  Serial.println("Server started");
 }
 
 void loop() {
@@ -43,13 +39,26 @@ void loop() {
       if (client.available()) {
         char c = client.read();
         Serial.write(c);
+        header += c;
         if (c == '\n') {
           if (currentLine.length() == 0) {
             client.println("HTTP/1.1 200 OK");
             client.println("Content-type:text/html");
             client.println();
-            client.print("<h1>Click <a href=\"/H\">here</a> turn the LED on</h1><br>");
-            client.print("<h1>Click <a href=\"/L\">here</a> turn the LED off</h1><br>");
+
+            if (header.indexOf("GET /H") >= 0) {
+              output12State = "on";
+              digitalWrite(output12, HIGH);
+            } else if (header.indexOf("GET /L") >= 0) {
+              output12State = "off";
+              digitalWrite(output12, LOW);
+            }
+
+            client.println("<!DOCTYPE html><html>");
+            client.println("<body><h1>ESP32 Web Server</h1>");
+            client.println("<p>Built in LED - State " + output12State + "</p>");
+            if (output12State == "off") client.println("<p><a href=\"/H\"><button class=\"button\">ON</button></a></p>");
+            else client.println("<p><a href=\"/L\"><button class=\"button button2\">OFF</button></a></p>");
             client.println();
             break;
           } else {
@@ -58,16 +67,11 @@ void loop() {
         } else if (c != '\r') {
           currentLine += c;
         }
-        if (currentLine.endsWith("GET /L")) {
-          digitalWrite(sensor, LOW);
-        }
-        if (currentLine.endsWith("GET /H")) {
-          digitalWrite(sensor, HIGH);
-        }
       }
     }
-    client.stop(); // Move this inside the if(client) block
-    Serial.println("Client stopped, what will you do you loser");
-    delay(5000);
+    header = "";
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
   }
 }
